c381442d602382b7932f765e838c88dfed760c0f
timfargo <isalintimur981@gmail.com>
2022-05-01 16:46:44 +0300

    Initial commit

---
+++ src/skypro/java/Main.java
@@ -0,0 +1,101 @@
+package skypro.java;
+
+public class Main {
+
+    public static void main(String[] agrs) {
+        // Задание 1
+        int clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Устанновите версию приложения для IOS по сылке");
+
+            if (clientOS == 1) {
+                System.out.println("Устанновите версию приложения для Android по сылке");
+            }
+        }
+    }
+
+    // Задание 2
+    int clientOS = 2017;
+    if (clientDeviceYear <= 2015 && clientOS ==0) {
+        System.out.println("Установите облегчонную версию приложения для IOS по сылке");
+        if (clientDeviceYear <= 2015 && clientOS == 1) {
+            System.out.println("Установите облегчонную версию приложения для Android по сылке");
+
+            // Заданте 3
+            int Year = 2020;
+            if (Year >= 2022) {
+                System.out.println("2020 год является высокостным");
+            } else {
+                System.out.println("2022 год не является высокостным");
+            // Задание 4
+            char deliveryDistance = 95;
+
+            if (deliveryDistance == 20) {
+                System.out.println("Доставка в пределах 20 км занимает сутки");
+            } else {
+                if (deliveryDistance >= 20) {
+                    System.out.println("Доставка в пределах от 20 км до 60 км добовляет еще один день доставки");
+                } else {
+                if (deliveryDistance >= 60) {
+                    System.out.println("Доставка в пределах 60 км до 100 км добовляет еще одни сутки");
+
+            // Заданте 5
+            int monthNumber = 12;
+
+            switch (monthNumber) {
+                case 12:
+                case 1:
+                case 2:
+                    System.out.println("Зимний сезон");
+                case 3:
+                case 4:
+                case 5:
+                    System.out.println("Весенний сезон");
+                case 6:
+                case 7:
+                case 8:
+                    System.out.println("Летний сезон");
+                case 9:
+                case 10:
+                case 11:
+                    System.out.println("Осенний сезон");
+                break;
+                if (monthNumber <= 12) {
+            }
+                }
+
+
+                }
+            }
+            }
+            }
+
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
